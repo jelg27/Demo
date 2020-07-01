a650b347f89b3f72fd13f1f4f4ec91ccac3a13cf
jelg27 <67451083+jelg27@users.noreply.github.com>
2020-07-01 03:09:07 -0500

    :9

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from "@angular/router";
+
+import { IndexComponent } from "./pages/index/index.component";
+import { aresComponent } from "./pages/examples/ares/ares.component";
+import { RegisterpageComponent } from "./pages/examples/registerpage/registerpage.component";
+import { LandingpageComponent } from "./pages/examples/landingpage/landingpage.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: IndexComponent },
+  { path: "ares", component: aresComponent },
+  { path: "register", component: RegisterpageComponent },
+  { path: "landing", component: LandingpageComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      useHash: true
+    })
+  ],
+  exports: []
+})
+export class AppRoutingModule {}
---
+++ src/app/pages/examples/ares/ares.component.ts
@@ -0,0 +1,123 @@
+import { Component, OnInit, OnDestroy } from "@angular/core";
+
+import * as CanvasJS from './canvasjs.min';
+
+@Component({
+  selector: "app-ares",
+  templateUrl: "ares.component.html"
+})
+export class aresComponent implements OnInit, OnDestroy {
+  isCollapsed = true;
+  constructor() {}
+
+  ngOnInit() {
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.add("ares");
+    let chart = new CanvasJS.Chart("chartContainer", {
+      theme: "dark2",
+      animationEnabled: true,
+      exportEnabled: true,
+      title:{
+        text: "HANGUP REASON"
+      },
+      data: [{
+        type: "pie",
+        showInLegend: true,
+        toolTipContent: "<b>{name}</b>: {y} calls (#percent%)",
+        indexLabel: "{name} - #percent%",
+        dataPoints: [
+          { y: 642, name: "CUSTOMER" },
+          { y: 492, name: "AGENT" },
+          { y: 99, name: "QUEUETIMEOUT" },
+          { y: 15, name: "ABANDON" },
+          { y: 151, name: "NO ANSWER" }
+        ]
+      }]
+    });
+      
+    chart.render();
+
+    let idchart = new CanvasJS.Chart("idChart", {
+      animationEnabled: true,
+      exportEnabled: true,
+      theme: "dark2",
+      title: {
+        text: "ID list calls"
+      },
+      axisY:{
+        tickLength: 10,
+        interval: 200
+      
+      },
+
+      data: [{
+        type: "column",
+        dataPoints: [
+          { y: 13, label: "995" },
+          { y: 1, label: "998" },
+          { y: 9, label: "Lista1" },
+          { y: 5, label: "Lista 2" },
+          { y: 1, label: "Lista 3" },
+          { y: 2, label: "Lista 4" },
+          { y: 580, label: "Lista 5" },
+          { y: 780, label: "Lista 6" },
+          ]
+      }]
+    });
+      
+    idchart.render();
+
+    let agents = new CanvasJS.Chart("agentsChart", {
+      zoomEnabled: true,
+      animationEnabled: true,
+      theme: "dark2",
+      exportEnabled: true,
+      title: {
+        text: "Promedio de duracion de llamada (segundos)"
+      },
+      subtitles:[{
+        text: "Por numero de agente"
+      }],
+      data: [
+      {
+        type: "line",                
+        dataPoints: [
+          { y: 57, label: "Agente 1" }, 
+          { y: 9, label: "Agente 2" }, 
+          { y: 13, label: "Agente 3" }, 
+          { y: 15, label: "Agente 4" }, 
+          { y: 23, label: "Agente 5" }, 
+          { y: 88, label: "Agente 6" }, 
+          { y: 8, label: "Agente 7" }, 
+          { y: 48, label: "Agente 8" }, 
+          { y: 46, label: "Agente 9" }, 
+          { y: 46, label: "Agente 10" }, 
+          { y: 58, label: "Agente 11" }, 
+          { y: 90, label: "Agente 12" }, 
+          { y: 68, label: "Agente 13" }, 
+          { y: 46, label: "Agente 14" }, 
+          { y: 40, label: "Agente 15" }, 
+          { y: 87, label: "Agente 16" }, 
+          { y: 78, label: "Agente 17" }, 
+          { y: 88, label: "Agente 18" }, 
+          { y: 70, label: "Agente 19" }, 
+          { y: 76, label: "Agente 20" }, 
+          { y: 228, label: "Agente 21" }, 
+          { y: 57, label: "Agente 22" }, 
+          { y: 21, label: "Agente 23" }, 
+          { y: 20, label: "Agente 24" }, 
+          { y: 30, label: "Agente 25" }, 
+          { y: 5, label: "Agente 26" }
+ ]
+
+      }]
+    });
+      
+    agents.render();
+    
+  }
+  ngOnDestroy() {
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.remove("ares");
+  }
+}
